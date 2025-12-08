c9b6301db178f9fc71807b501671f698ec8d8760
nguyenkhoiphuquy2-hue <nguyenkhoiphuquy2@gmail.com>
2025-12-08 19:19:50 +0700

    khoi

--- chitietimg/chitiet.php
+++ chitietimg/chitiet.php
@@ -14,7 +14,7 @@
 <div class="container">
 
     <div class="thumbs">
-        <img src="img/1.png">
+        <img src="img/1.avif">
         <img src="img/2.png">
         <img src="img/3.png">
         <img src="img/4.png">
